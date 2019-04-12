424e721e08f4ade0830da933e9f2ccc76bffe9ca
unknown <getulio222@hotmail.com>
2019-04-11 21:38:36 -0300

    Projeto criado

---
+++ src/main/java/com/getulioazevedo/aulasmc/AulasmcApplication.java
@@ -0,0 +1,13 @@
+package com.getulioazevedo.aulasmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getulioazevedo/aulasmc/AulasmcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.getulioazevedo.aulasmc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AulasmcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
