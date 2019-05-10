72aba878113a4648e12453f82f0ee29e8d1ac776
Getulio Azevedo <getulio222@hotmail.com>
2019-05-09 21:50:17 -0300

    Criando operacao de instanciacao

--- src/main/java/com/getulioazevedo/aulasmc/AulasmcApplication.java
+++ src/main/java/com/getulioazevedo/aulasmc/AulasmcApplication.java
@@ -1,13 +1,37 @@
 package com.getulioazevedo.aulasmc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AulasmcApplication {
+import com.getulioazevedo.aulasmc.domain.Categoria;
+import com.getulioazevedo.aulasmc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AulasmcApplication implements CommandLineRunner {
+
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AulasmcApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository repo;
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Getulio");
+		Categoria cat2 = new Categoria(null,"Ayllinha");
+		
+		repo.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+		
 	}
 
 }
